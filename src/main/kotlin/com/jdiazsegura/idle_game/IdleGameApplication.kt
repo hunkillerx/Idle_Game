1a375c9fe9956a644451f6b9aa3adcbfcde22b5c
Jose Diaz <josediaz4297@gmail.com>
2024-11-07 10:29:53 -0500

    Initial commit

---
+++ src/main/kotlin/com/jdiazsegura/idle_game/IdleGameApplication.kt
@@ -0,0 +1,11 @@
+package com.jdiazsegura.idle_game
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IdleGameApplication
+
+fun main(args: Array<String>) {
+    runApplication<IdleGameApplication>(*args)
+}
---
+++ src/test/kotlin/com/jdiazsegura/idle_game/IdleGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jdiazsegura.idle_game
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IdleGameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
